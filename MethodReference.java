4f6f6f2c320cda658e67ffa332268156d1538c7f
Manjupriya-P <91693921+Manjupriya-P@users.noreply.github.com>
2022-03-31 15:51:23 +0530

    Method Reference

---
+++ MethodReference.java
@@ -0,0 +1,11 @@
+package com.mischallaneous.Mischallaneous;
+@FunctionalInterface
+public interface MethodReference {
+
+	
+	
+	
+	void method1();
+//default methods can be declared only in a functional interface
+//when we assign this to a method in a class we can implementation
+}
